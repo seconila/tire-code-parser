b67ec6c8a4451c267f0d879b981bfe796a9219fd
Seconila <24533593+seconila@users.noreply.github.com>
2019-02-16 08:32:25 +0000

    Update documentation for Parser class

--- src/Seconila/TireCode/Parser.php
+++ src/Seconila/TireCode/Parser.php
@@ -5,6 +5,7 @@ namespace Seconila\TireCode;
 class Parser
 {
     /**
+     * Passenger car tires
      * https://en.wikipedia.org/wiki/Tire_code
      *
      * E.g. 225/55 R17 101V S954 TL XL
@@ -12,6 +13,11 @@ class Parser
      */
     protected $tire_code;
 
+    /**
+     * Passenger car tire code string e.g. 225/55 R17 101V S954 TL XL
+     * TODO: Validate for acceptable characters only
+     * @param string $tire_code
+     */
     public function __construct($tire_code)
     {
         $this->tire_code = trim($tire_code);
@@ -23,6 +29,11 @@ class Parser
         return $this->tire_code;
     }
 
+    /**
+     * 3-digit number: The "nominal section width" of the tire in millimeters;
+     * the widest point from both outer edges (side wall to side wall).
+     * @return mixed
+     */
     public function getWidth()
     {
         if (preg_match("/^([0-9]+)\/[0-9]+/", $this->tire_code, $matches)) {  // Check if begins with "225"
@@ -30,6 +41,11 @@ class Parser
         }
     }
 
+    /**
+     * 2- or 3-digit number: The "aspect ratio" of the sidewall height as a
+     * percentage of the nominal section width of the tire.
+     * @return mixed
+     */
     public function getAspectRatio()
     {
         if (preg_match("/^[0-9]+\/([0-9]+)/", $this->tire_code, $matches)) {
@@ -54,6 +70,11 @@ class Parser
         }
     }
 
+    /**
+     * 1- or 2-digit number: Diameter in inches of the wheel that the tires are
+     * designed to fit.
+     * @return mixed
+     */
     public function getRimDiameter()
     {
         if (preg_match("/^[0-9]+\/[0-9]+\s*[{R,ZR}]+\s*([0-9]{2})/", $this->tire_code, $matches)) {
@@ -61,6 +82,12 @@ class Parser
         }
     }
 
+    /**
+     * 2- or 3-digit number: Load index. The load index on a passenger-car tire
+     * is a numerical code stipulating the maximum load (mass, or weight) each
+     * tire can carry.
+     * @return mixed
+     */
     public function getLoadIndex()
     {
         $load_index_speed_symbol = $this->getLoadIndexSpeedSymbol();
@@ -69,15 +96,22 @@ class Parser
         }
     }
 
+    /**
+     * Combined LoadIndex and SpeedSymbol.
+     * @return mixed
+     */
     public function getLoadIndexSpeedSymbol()
     {
         $pattern = "/^[0-9]+\/[0-9]+\s*[{R,ZR}]+\s*[0-9]{2}\s*(\(?[0-9]{2,3}[QRSTHVWY]\)?)/";
-
         if (preg_match($pattern, $this->tire_code, $matches)) {
             return $matches[1];
         }
     }
 
+    /**
+     * The speed symbol is made up of a single letter - Q, R, S, T, H, V, W, Y.
+     * @return mixed
+     */
     public function getSpeedSymbol()
     {
         $load_index_speed_symbol = $this->getLoadIndexSpeedSymbol();
