2257a2a608239b7e4596cf80d75d3d224736018d
Seconila <24533593+seconila@users.noreply.github.com>
2019-02-24 14:28:51 +0000

    Add more tests for better coverage

--- tests/src/Seconila/TireCode/ParserTest.php
+++ tests/src/Seconila/TireCode/ParserTest.php
@@ -21,6 +21,9 @@ class ParserTest extends TestCase
 
         $parser = new Parser(' 185/60 R14 82H 86A TL  ');
         $this->assertEquals('185', $parser->getWidth());
+
+        $parser = new Parser('XXX/55 R17 101V S954 TL XL');
+        $this->assertEquals(null, $parser->getWidth());
     }
 
     public function testGetAspectRatio()
@@ -30,6 +33,9 @@ class ParserTest extends TestCase
 
         $parser = new Parser(' 185/60 R14 82H 86A TL  ');
         $this->assertEquals('60', $parser->getAspectRatio());
+
+        $parser = new Parser('225/XX R17 101V S954 TL XL');
+        $this->assertEquals(null, $parser->getAspectRatio());
     }
 
     public function testGetSpeedRating()
@@ -48,6 +54,9 @@ class ParserTest extends TestCase
 
         $parser = new Parser('275/35ZR19 100Y PXT1R TL        ');
         $this->assertEquals('19', $parser->getRimDiameter());
+
+        $parser = new Parser('275/35ZRXX 100Y PXT1R TL');
+        $this->assertEquals(null, $parser->getRimDiameter());
     }
 
     public function testGetTireSize()
@@ -57,6 +66,9 @@ class ParserTest extends TestCase
 
         $parser = new Parser('275/35ZR19 100Y PXT1R TL        ');
         $this->assertEquals('275/35', $parser->getTireSize());
+
+        $parser = new Parser('XXX/XXZR19 100Y PXT1R TL');
+        $this->assertEquals(null, $parser->getTireSize());
     }
 
     public function testGetLoadIndexEnclosedInBrackets()
@@ -69,12 +81,21 @@ class ParserTest extends TestCase
     {
         $parser = new Parser('275/35 ZR19 (100Y) XL');
         $this->assertEquals('100', $parser->getLoadIndex());
+
+        $parser = new Parser('275/35 ZR19 (Y) XL');
+        $this->assertEquals(null, $parser->getLoadIndex());
+
+        $parser = new Parser('275/35 ZR19 XL');
+        $this->assertEquals(null, $parser->getLoadIndex());
     }
 
     public function testGetSpeedSymbolNotEnclosedInBrackets()
     {
         $parser = new Parser('225/55 R17 101V S954 TL XL');
         $this->assertEquals('V', $parser->getSpeedSymbol());
+
+        $parser = new Parser('225/55 R17 101 S954 TL XL');
+        $this->assertEquals(null, $parser->getSpeedSymbol());
     }
 
     public function testGetSpeedSymbolEnclosedInBrackets()
@@ -87,6 +108,12 @@ class ParserTest extends TestCase
     {
         $parser = new Parser('225/55 R17 101V S954 TL XL');
         $this->assertEquals('101V', $parser->getLoadIndexSpeedSymbol());
+
+        $parser = new Parser('225/55 R17 S954 TL XL');
+        $this->assertEquals(null, $parser->getLoadIndexSpeedSymbol());
+
+        $parser = new Parser('225/55 R17 XXV S954 TL XL');
+        $this->assertEquals(null, $parser->getLoadIndexSpeedSymbol());
     }
 
 }
